69b2c1e39c2c8577ee50d3d33b2609ab3934427a
buhohacker <andreadelnido@gmail.com>
2022-03-27 12:14:29 +0200

    Dos props

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,17 +4,22 @@ import ReactDOM from 'react-dom'
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="George" />
-      <Hello name="Daisy" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
