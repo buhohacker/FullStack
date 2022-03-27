b9c8cbd2bdf53be075f2747ecc5f5237958a824e
buhohacker <andreadelnido@gmail.com>
2022-03-27 11:53:22 +0200

    Commponentes múltiples 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
   )
 }
 
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById('root')
-)
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
