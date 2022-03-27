89a361f1a23c2cf16d51cc6bfbb19eb4e660cebe
buhohacker <andreadelnido@gmail.com>
2022-03-27 13:10:21 +0200

    Ejercicio 1.1 terminado

--- part1/exercises_part1/src/index.js
+++ part1/exercises_part1/src/index.js
@@ -29,7 +29,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.total}</p>
     </div>
   )
 }
