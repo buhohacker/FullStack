c3688f8b88816200425c8a80d821d4ed66003b00
buhohacker <andreadelnido@gmail.com>
2022-03-27 12:31:52 +0200

    Footer

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,6 +11,14 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/buhohacker">buhohacker</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = 'Peter'
   const age = 10
@@ -20,6 +28,7 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      <Footer />
     </div>
   )
 }
